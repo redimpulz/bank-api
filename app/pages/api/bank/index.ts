e28871b0261f5735770b104dbce73b20d0f0dc38
Ken Takahashi <takaken@redimpulz.com>
2021-08-15 17:27:49 +0900

    リファクタリング

--- app/pages/api/bank/index.ts
+++ app/pages/api/bank/index.ts
@@ -1,7 +1,7 @@
 import handler from '@/server/api/handler';
 import zenginCode from 'zengin-code';
 
-export default handler().get((req, res) => {
+export default handler().get((_, res) => {
   const data = Object.entries(zenginCode).map(([, value]) => ({
     code: value.code,
     name: value.name,
